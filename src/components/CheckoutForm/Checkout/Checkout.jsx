7b259cfea08e48644ea0ac6d6618682e90516a3f
Riya-Anadkat <riya.anadkat@gmail.com>
2021-01-02 21:31:39 -0500

    payment form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,68 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core'
-// import useStyles from './styles';
-// import DetailsForm from '../DetailsForm';
-// import PaymentForm from '../PaymentForm';
-// import { commerce } from '../../../lib/commerce';
-
-// const steps = ['Customer Details','Payment']
-
-// const Checkout = ( {cart} ) => {
-//     const [activeStep, setActiveStep] = useState(0);
-//     const classes = useStyles();
-
-//     const Confirmation = () => (
-//         <div>
-//             Confirmation
-//         </div>
-//     )
-
-//     const [checkoutToken, setCheckoutToken] = useState(null);
-
-//     useEffect(() => {
-//       const generateToken = async() => {
-//         try {
-//           const token = await commerce.checkout.generateToken(cart.id, { type: 'cart'});
-//           console.log(token);
-
-//           setCheckoutToken(token);
-          
-//         } catch (error) {
-          
-//         }
-//       }
-//       generateToken();
-//     }, [cart]);
-
-//     const Form = () => activeStep === 0
-//         ? <DetailsForm checkoutToken={checkoutToken} />
-//         : <PaymentForm />
-
-//     return (
-//         <>
-//         <div className={classes.toolbar} />
-//         <main className={classes.layout}>
-//             <Paper className={classes.paper}>
-//                 <Typography variant="h3" align="center">Checkout</Typography>
-//                 <Stepper activeStep={activeStep} className={classes.stepper}>
-//                     {steps.map((step) => (
-//                         <Step key={step}>
-//                             <StepLabel>{step}</StepLabel>
-//                         </Step>
-//                     ))}
-//                 </Stepper>
-//                 {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
-                
-                
-//             </Paper>
-//         </main>
-//         </>
-//     )
-// }
-
-// export default Checkout
-
-
 import React, { useState, useEffect } from 'react';
 import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import { Link, useHistory } from 'react-router-dom';
@@ -77,7 +12,7 @@ const steps = ['Customer Order Details', 'Payment details'];
 const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
   const [checkoutToken, setCheckoutToken] = useState(null);
   const [activeStep, setActiveStep] = useState(0);
-  const [shippingData, setShippingData] = useState({});
+ 
   const classes = useStyles();
   const history = useHistory();
 
@@ -101,7 +36,6 @@ const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
   }, [cart]);
 
   const test = (data) => {
-    setShippingData(data);
 
     nextStep();
   };
@@ -131,8 +65,9 @@ const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
     );
   }
   const Form = () => (activeStep === 0
-    ? <AddressForm checkoutToken={checkoutToken} nextStep={nextStep} setShippingData={setShippingData} test={test} />
-    : <PaymentForm checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} shippingData={shippingData} onCaptureCheckout={onCaptureCheckout} />);
+    ? <AddressForm checkoutToken={checkoutToken} nextStep={nextStep} test={test} />
+    : <PaymentForm checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout} />);
+
   return (
     <>
       <CssBaseline />
--- src/components/CheckoutForm/DetailsForm.jsx
+++ src/components/CheckoutForm/DetailsForm.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState}  from 'react';
+import React, { useEffect, useState } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 import FormInput from './CustomTextField';
@@ -10,73 +10,74 @@ import FileBase from 'react-file-base64';
 const DetailsForm = ({ checkoutToken, test }) => {
 
   const methods = useForm();
- 
+
 
   const ExtraField = ({ extrafield }) => {
-      return (
-        <div>
-        {extrafield.name == "Image" ? 
+    return (
+      <div>
+        {extrafield.name == "Image" ?
           <div>
-            Upload Image for Customs 
+            Upload Image for Customs
             <br />
-            
+
             {/* <FileBase type= "file" multiple={false} onDone={({base64}) => setPostData({...postData, selectedFile: base64})}/> */}
-            <FileBase 
-              type= "file" 
+            <FileBase
+              type="file"
               multiple={false}
 
             />
-            
+
           </div>
-        :
-        <div> 
-            <FormInput 
-            
-            required = {extrafield.required}
-            name = {extrafield.name}
-            label={extrafield.name} 
+          :
+          <div>
+            <FormInput
+
+              required={extrafield.required}
+              name={extrafield.name}
+              label={extrafield.name}
 
             />
-        </div>
+          </div>
         }
-        </div>
-      )
+      </div>
+    )
   }
   const Products = ({ product }) => {
     return (
       <div>
-      <br />
-      <Typography variant="subtitle1" gutterBottom >Details for Item: {product.name}</Typography>
+        <br />
+        <Typography variant="subtitle1" gutterBottom >Details for Item: {product.name}</Typography>
         {product.extrafields.map((extrafield) => (
-            <ExtraField extrafield={ extrafield } />
-        
-            ))}
-      </div>
-      
-    )
-}
+          <ExtraField extrafield={extrafield} />
 
-    return (
-<>
-    <Typography variant="h6" gutterBottom align="center">Customer Details</Typography>
+        ))}
+      </div>
+
+    )
+  }
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom align="center">Customer Details</Typography>
       <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit((data) => test({ ...data}))}>
+        <form onSubmit={methods.handleSubmit((data) => test({ ...data }))}>
+        {/* spreading data into an object */}
           <Grid >
             <FormInput required name="firstName" label="First name" />
             <FormInput required name="lastName" label="Last name" />
             <FormInput required name="email" label="Email" />
- 
+
             <br />
-            
+
             <Typography variant="h6" gutterBottom align="center">Order Details</Typography>
-            
-                {checkoutToken.products.map((product) => (
-                        <Grid item key = {checkoutToken.id} >
-                          <Products product={ product } />
-                        </Grid>
-                         ))}
-             
-          </Grid> 
+
+            {checkoutToken.products.map((product) => (
+              <Grid item key={checkoutToken.id} >
+                <Products product={product} />
+              </Grid>
+            ))}
+
+          </Grid>
           <br />
 
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -87,7 +88,7 @@ const DetailsForm = ({ checkoutToken, test }) => {
       </FormProvider>
     </>
 
-    )
+  )
 }
 
 export default DetailsForm
